57ceae4b1c435b4d91b764e86585eeaa8af99eac
adamjacobbecker <ad@mbecker.cc>
2012-10-22 13:43:13 -0400

    allow for multiple oauth providers

--- models/user.js
+++ models/user.js
@@ -3,8 +3,11 @@ var mongoose = require('mongoose');
 var schema = new mongoose.Schema({
   is_admin: Boolean,
   dataset_permissions: Array,
-  email: {type: String, index: true},
-  token: {type: String, index: true}
+  email: String,
+  token: String,
+  provider: String
 });
+
+schema.index({provider: 1, token: 1}, {unique: true});
 
 module.exports = DB.model('User', schema);
